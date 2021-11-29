1bf9debd3c931a2a5d8f91300f27b8a62b6d27b4
Dan Elms <352577@edu.cwa.ac.uk>
2021-11-29 16:02:38 +0000

    GP - addSale unresolved external symbol error

--- CarSales/CarSales/CarSales.cpp
+++ CarSales/CarSales/CarSales.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <Windows.h>
 #include "ReadLine.h"
 #include "Car.cpp"
 #include "Sales.cpp"
@@ -10,12 +11,15 @@ void adminMenu();
 void addStock();
 void removeStock();
 void viewSales();
+void addSale(char* _custName, int _custAge, char* _custEmail, char* _newMake, char* _newModel, int _newAmount, double _newPrice, double _newOffer, bool _newFinal);
 void loadFiles();
 void saveFiles();
 void BubbleSortDescStock(Car* _cars[]);
+Date getDate();
 
 #define MAX_LEN 255
-#define ARRAY_SIZE(x) sizeof(x) / sizeof(x[0])
+
+SYSTEMTIME _sysDate;
 
 Car* _stock[10] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 Sale* _sales[];
@@ -23,7 +27,7 @@ Sale* _sales[];
 FILE* _fptr;
 
 const char* _adminPassword = "hardPasswordToGuess123!#";
-int i, _carIndex = 0;
+int i, _carIndex = 0, _salesIndex = 0;
 
 int main()
 {
@@ -75,7 +79,7 @@ void viewCars()
         if (_car != NULL)
         {
             _inStock = true;
-            printf("%d. %s %s\t\t\tIn Stock: %d\n",_car->getMenuPlace(), _car->getMake(), _car->getModel(), _car->getStock());
+            printf("%d. %s %s. In Stock: %d\n",_car->getMenuPlace(), _car->getMake(), _car->getModel(), _car->getStock());
         }
     }
 
@@ -88,7 +92,7 @@ void viewCars()
 void buyCars()
 {
     viewCars();
-    printf("4. Return to Main Menu\n\n");
+    printf("%d. Return to Main Menu\n\n", _carIndex + 1);
 
     int _selection = readIntInRange("Select the car you would like to purchase: ",1,_carIndex + 1);
     bool _quit = false, _currentSet = false;
@@ -104,7 +108,7 @@ void buyCars()
         {
             for (Car* _car : _stock)
             {
-                if (_selection == _car->getMenuPlace())
+                if (NULL != _car &&_selection == _car->getMenuPlace())
                 {
                     _currentSelection = _car;
                     _currentSet = true;
@@ -117,7 +121,7 @@ void buyCars()
                 _quit = true;
             }
 
-            int _amount = readInt("How many would you like to purchase?: ");
+            int _amount = readInt("\nHow many would you like to purchase?: ");
             double _offer;
 
             if (_amount > _currentSelection->getStock())
@@ -131,10 +135,38 @@ void buyCars()
             else
             {
                 _currentSelection->setStock(_currentSelection->getStock() - _amount); //Remove amount desired from current stock
-                _offer = readDouble("Enter the full price* to complete purchase now, or make an offer\n*Price per car: ");
+                printf("\nAsking price: %.2lf\n", _currentSelection->getPrice());
+                _offer = readDouble("\nEnter the full asking price to complete purchase now, or make an offer\nYour offer (per car): ");
+                char* _name{};
+                char* _email{};
+                int _age;
 
-                //CREATE SALES RECORD FULL PRICE (SOLD)
-                //CREATE SALES RECORD OFFER (PENDING)
+                if (_offer > _currentSelection->getPrice())
+                {
+                    printf("\nError: Sorry, we cannot accept offers over the asking price\nPlease enter the full asking price or make an offer\n");
+                }
+                else if (_offer < _currentSelection->getPrice())
+                {
+                    printf("\nPlease provide some details so we can get in touch and improve analytics\nPlease ensure these details are correct\n");
+                    readString("Your name: ", _name, MAX_LEN);
+                    readString("Your email address: ", _email, MAX_LEN);
+                    _age = readIntInRange("Your age: ", 1, 130);
+
+                    addSale(_name, _age, _email, _currentSelection->getMake(), _currentSelection->getModel(), _amount, _currentSelection->getPrice(), _offer, false);
+                    printf("\nThank you!\nYour offer has been recorded and is pending confirmation\nWe will contact you by email shortly\n");
+                }
+                else if (_offer == _currentSelection->getPrice())
+                {
+                    printf("\nPlease provide some details so we can get in touch and improve analytics\nPlease ensure these details are correct\n");
+                    readString("Your name: ", _name, MAX_LEN);
+                    readString("Your email address: ", _email, MAX_LEN);
+                    _age = readIntInRange("Your age: ", 1, 130);
+
+                    addSale(_name, _age, _email, _currentSelection->getMake(), _currentSelection->getModel(), _amount, _currentSelection->getPrice(), _offer, true);
+                    printf("\nThank you! Your offer has been accepted\nWe will contact you by email shortly\n");
+                }
+
+                _quit = true;
             }
         }
         
@@ -189,6 +221,8 @@ void addStock()
     _newCar->setStock(_inStock);
 
     _stock[_carIndex] = _newCar;
+
+    _carIndex++;
 }
 
 void removeStock()
@@ -199,6 +233,44 @@ void removeStock()
 void viewSales()
 {
     
+}
+
+void addSale(char* _custName, int _custAge, char* _custEmail, char* _newMake, char* _newModel, int _newAmount, double _newPrice, double _newOffer, bool _newFinal) //Creates and adds a sale to _sales array
+{
+   //OG ATTEMPT
+    
+   //Sale* _sale = new Sale;
+
+   /* _sale->_nameOfCust = _custName;
+    _sale->_ageOfCust = _custAge;
+    _sale->_emailAddOfCust = _custEmail;
+
+    _sale->_make = _newMake;
+    _sale->_model = _newModel;
+    _sale->_price = _newPrice;
+    _sale->_offer = _newOffer;
+    _sale->_final = _newFinal;
+    
+    _sale->_dateOfSale = getDate();
+    
+    _sales[_salesIndex] = _sale;*/
+
+
+    //NEW ATTEMPT
+
+    _sales[_salesIndex]->_nameOfCust = _custName;
+    /*_sales[_salesIndex]->_ageOfCust = _custAge;
+    _sales[_salesIndex]->_emailAddOfCust = _custEmail;
+    _sales[_salesIndex]->_make = _newMake;
+    _sales[_salesIndex]->_model = _newModel;
+    _sales[_salesIndex]->_amount = _newAmount;
+    _sales[_salesIndex]->_price = _newPrice;
+    _sales[_salesIndex]->_offer = _newOffer;
+    _sales[_salesIndex]->_final = _newFinal;
+
+    _sales[_salesIndex]->_dateOfSale = getDate();
+
+    _salesIndex++;*/
 }
 
 void loadFiles()
@@ -315,4 +387,14 @@ void BubbleSortDescStock(Car* _cars[])
             }
         }
     }
+}
+
+Date getDate() //Creates and returns a Date struct containing today's date
+{
+    Date* _date = new Date;
+    _date->_day = _sysDate.wDay;
+    _date->_month = _sysDate.wMonth;
+    _date->_year = _sysDate.wYear;
+
+    return *_date;
 }
--- CarSales/CarSales/Sales.cpp
+++ CarSales/CarSales/Sales.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 
-struct Date //Struct used to store the date of a sale
+struct Date //Struct used to store the date of a sale / offer
 {
 	int _day;
 	int _month;
 	int _year;
 };
 
-struct Sale //Struct to store details of a sale (What car sold, for how much and when)
+struct Sale //Struct to store details of a sale (What car sold, for how much, when, and to whom)
 {
-	char* _make;
+	char* _nameOfCust; //Customer Name	
+	int _ageOfCust; //Customer Age
+	char* _emailAddOfCust; //Customer Email Address
+
+	char* _make; 
 	char* _model;
-	float _price;
-	float _salePrice;
+	int _amount;
+	double _price;
+	double _offer;
 	Date _dateOfSale;
+
+	bool _final; //Bool to differentiate between pending and finalised sales
 };
